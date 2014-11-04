67921e94b2350367d85702d5650aa492385cb801
kar-javd <kar.javadyan@plexonic.com>
2014-11-05 01:50:25 +0300

    change python interpreter to 3,4

--- scr/mainFile.py
+++ scr/mainFile.py
@@ -1,1 +1,33 @@
 __author__ = 'Java'
+k = input()
+nextInput = input()
+nextInputTokens = nextInput.split()
+deSortArray = []
+for item in nextInputTokens:
+   deSortArray.append(int(item))
+def mySuperFunction(deSortedArray):
+    counts = [0 for i in range(0,10)]
+
+    for item in deSortedArray:
+        counts[item%10]+=1
+    outputStr = ""
+    for i in range(1,10):
+        for j in range(0,counts[i]):
+            outputStr +=str(i)
+            outputStr+=' '
+    for j in range(0,counts[0]):
+        outputStr+=str(10)
+        outputStr+= ' '
+    return outputStr
+
+
+
+print(mySuperFunction(deSortArray))
+
+
+
+
+
+
+
+
