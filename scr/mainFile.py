d5dd6db2f0f74ffdcb02c804772be922eb6090c7
kar-javd <kar.javadyan@plexonic.com>
2014-11-08 02:38:36 +0300

    merge sort and inversions count implementation

--- scr/mainFile.py
+++ scr/mainFile.py
@@ -1,46 +1,62 @@
-__author__ = 'Java'
-
 kInput = input()
 arrayInput = input()
 arrayInputTokens = arrayInput.split()
 arrayBeforeSort = [int(item) for item in arrayInputTokens]
 
-
 def sortAndCountInversions(nonSortArray):
     if len(nonSortArray) <= 1:
-        print("O_I")
         return 0
-    blueArray = [item for item in nonSortArray[:len(nonSortArray) // 2]]
-    greenArray = [item for item in nonSortArray[len(nonSortArray) // 2:len(nonSortArray)]]
+    if len(nonSortArray) == 2:
+        if nonSortArray[0]>nonSortArray[1]:
+            nonSortArray.sort()
+            return 1
+        else:
+            return 0
+    print("NON SORTED ARRAY"+str(nonSortArray))
 
-    print("Blue array before sorting:"+str(blueArray))
-    print("Green array before sorting:"+str(greenArray))
+    blueArray = nonSortArray[:(len(nonSortArray)+1) // 2]
+    greenArray = nonSortArray[(len(nonSortArray)+1) // 2:]
+
+    print("NON SORTED BLUE ARRAY"+str(blueArray))
+    print("NON SORTED GREEN ARRAY"+str(greenArray))
 
     inversionsInBlue = sortAndCountInversions(blueArray)
     inversionsInGreen = sortAndCountInversions(greenArray)
-
-    print("Blue array after sorting:"+str(blueArray))
-    print("Green array after sorting:"+str(greenArray))
 
     sortedArray = []
     blueGreenInversionsCount = 0
     i = 0
     j = 0
+    # blueArray.sort()
+    # greenArray.sort()
+    print("SORTED BLUEARRAY"+str(blueArray))
+    print("SORTED GREENARRAY"+str(greenArray))
+    for k in range(0,len(nonSortArray)):
+        if(i == len(blueArray)):
+            for item in greenArray[j:]:
+                sortedArray.append(item)
+            break
+        if(j == len(greenArray)):
+            for item in blueArray[i:]:
+                sortedArray.append(item)
+            break
 
-    for k in range(0, len(blueArray)+len(greenArray)):
-        if i !=len(blueArray) and blueArray[i] <= greenArray[j]:
+        if blueArray[i] <= greenArray[j]:  #"""i<len(blueArray)) and"""
             sortedArray.append(blueArray[i])
             i += 1
         else:
             sortedArray.append(greenArray[j])
             blueGreenInversionsCount += (len(blueArray) - i)
             j += 1
-    print("Number in nonsortedArray:"+str(nonSortArray)+"is "+str(inversionsInBlue + inversionsInGreen + blueGreenInversionsCount))
-    nonSortArray = sortedArray
+    print("SortedARRAY:"+str(sortedArray));
+
+    nonSortArray.clear()
+    for item in sortedArray:
+         nonSortArray.append(item)
     return inversionsInBlue + inversionsInGreen + blueGreenInversionsCount
 
-print("------------------------------------------------------------------------")
 print(sortAndCountInversions(arrayBeforeSort))
+
 
 
 
